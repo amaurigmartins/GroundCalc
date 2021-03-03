a07b1767e913ca46963f6b95ff52abb5f2f19d55
amaurigmartins <amaurigmartins@gmail.com>
2021-03-03 16:21:29 -0300

    First upload

---
+++ functions/runGroundCalcSolver.m
@@ -0,0 +1,7 @@
+function [out] = runGroundCalcSolver(fname, cond_radius, rho_top, rho_bottom, h_top, i_src, t_fault, rho_cover, h_cover, plot_surf, plot_curr, plot_touch, plot_step)
+
+
+out = 10 * x;
+
+end
+
