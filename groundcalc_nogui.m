7d0f8e57a5aaf523d9eb0160b182a87d120e70d5
amaurigmartins <amaurigmartins@gmail.com>
2021-03-04 23:51:18 -0300

    Minor improvements

--- groundcalc_nogui.m
+++ groundcalc_nogui.m
@@ -3,9 +3,10 @@ close all;
 clc;
 
 addpath('functions');
-addpath('assets');
 
 fname='Drawing1.txt';
+delta=.5;
+buffer=10;
 cond_radius=0.0067056;
 rho_top=1000;
 rho_bottom=100;
@@ -18,8 +19,9 @@ plot_surf=true;
 plot_curr=true;
 plot_touch=true;
 plot_step=true;
+plot_grid=true;
 
 try
-    runGroundCalcSolver(fname, cond_radius, rho_top, rho_bottom, h_top, i_src, t_fault, rho_cover, h_cover, plot_surf, plot_curr, plot_touch, plot_step);
+    runGroundCalcSolver(fname, delta, buffer, cond_radius, rho_top, rho_bottom, h_top, i_src, t_fault, rho_cover, h_cover, plot_surf, plot_curr, plot_touch, plot_step, plot_grid);
 catch err
 end
